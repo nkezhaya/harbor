f22c77aba9fc8e1f43a8f2d6aeab572d17101a84
Nick Kezhaya <nick@whitepaperclip.com>
2025-09-17 11:45:43 -0500

    Use JSON instead of Jason

--- mix.exs
+++ mix.exs
@@ -63,7 +63,6 @@ defmodule Harbor.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:waffle, "~> 1.1"},
