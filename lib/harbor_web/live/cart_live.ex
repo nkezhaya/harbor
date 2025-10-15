ed617af970d275ec2cdd35513225a99eaa978b51
Nick Kezhaya <nick@whitepaperclip.com>
2025-10-15 10:28:17 -0500

    Placeholder Cart, Checkout views

---
+++ lib/harbor_web/live/cart_live.ex
@@ -0,0 +1,20 @@
+defmodule HarborWeb.CartLive do
+  @moduledoc """
+  Placeholder view for the shopping cart page.
+  """
+  use HarborWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash} current_scope={@current_scope} root_categories={@root_categories}>
+      <h1>Cart</h1>
+    </Layouts.app>
+    """
+  end
+end
