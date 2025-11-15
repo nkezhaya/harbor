0bc61e03d1006b25be9c7a39805c1fb0eb259994
Nick Kezhaya <20113+nkezhaya@users.noreply.github.com>
2025-11-15 11:38:35 -0600

    Remove phone field

--- lib/harbor_web/live/checkout_live/form.ex
+++ lib/harbor_web/live/checkout_live/form.ex
@@ -71,19 +71,6 @@ defmodule HarborWeb.CheckoutLive.Form do
           </div>
         </div>
 
-        <div class="mt-6">
-          <label for="phone" class="block text-sm/6 font-medium text-gray-700">Phone number</label>
-          <div class="mt-2">
-            <input
-              id="phone"
-              type="text"
-              name="phone"
-              autocomplete="tel"
-              class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-            />
-          </div>
-        </div>
-
         <button
           type="submit"
           disabled
